33a451147b7bddd2c75e0ebfb1cf75bfb4978eb1
Raghav <singh.raghverndra@pwc.com>
2017-02-03 11:58:02 +0800

    webpack basic version

---
+++ script/app.js
@@ -0,0 +1,2 @@
+const mssg = require('./message')
+alert(mssg)
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+  // Define entry point
+  entry: './script/app.js',
+
+  // Define output
+  output: {
+    path: 'dist',
+    filename: 'bundle.js',
+  }
+};
